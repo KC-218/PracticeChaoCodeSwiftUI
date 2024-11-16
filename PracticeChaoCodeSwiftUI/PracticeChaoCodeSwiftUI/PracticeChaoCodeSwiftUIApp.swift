02e804abb49118ba8ca3eef5592b9d1e474f0dee
KC <kc995266@gmail.com>
2024-11-16 20:36:04 +0800

    [新增] 專案初始化

---
+++ PracticeChaoCodeSwiftUI/PracticeChaoCodeSwiftUI/PracticeChaoCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeChaoCodeSwiftUIApp.swift
+//  PracticeChaoCodeSwiftUI
+//
+//  Created by Bruce Chen on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct PracticeChaoCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
