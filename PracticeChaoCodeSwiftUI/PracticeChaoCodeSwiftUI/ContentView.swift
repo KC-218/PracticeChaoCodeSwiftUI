fe236993d09949927059b7bb08314e4d1380c11e
KC <kc995266@gmail.com>
2024-11-17 17:32:26 +0800

    實作第一個畫面：今天吃什麼 - SwiftUI 新手入門

--- PracticeChaoCodeSwiftUI/PracticeChaoCodeSwiftUI/ContentView.swift
+++ PracticeChaoCodeSwiftUI/PracticeChaoCodeSwiftUI/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let foodArray = ["漢堡", "沙拉", "披薩", "義大利麵", "雞腿便當", "刀削麵", "火鍋", "牛肉麵", "關東煮"]
+    
+    @State private var selectedFood: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(spacing: 30) {
+            
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("今天吃什麼")
+                .font(.title)
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.green)
+            }
+            
+            
+            Button {
+                selectedFood = foodArray.shuffled().first ?? ""
+            } label: {
+                Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
+            }
+            .padding(.bottom, -15)
+            
+            Button {
+                selectedFood = .none
+            } label: {
+                Text("重置").frame(width: 200)
+            }
+            .buttonStyle(.bordered)
+            
         }
         .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color(uiColor: .secondarySystemBackground))
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .animation(.easeInOut, value: selectedFood)
+        
     }
 }
 
